2f8fc12aef4525c189a328a9306220fda14d4874
younesboulkaddid <boulkaddid.younes@gmail.com>
2016-02-06 22:52:16 +0100

    [FEATURE] Add router

---
+++ src/Controller/Home.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Younes
+ * Date: 06/02/2016
+ * Time: 22:27
+ */
+
+class Home{
+
+	public function index(){
+
+	}
+}
---
+++ src/Controller/lol.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class lol{
+
+
+	public function index(){
+		return 'index';
+	}
+	public function test(){
+		return 'kfshdc';
+	}
+}
